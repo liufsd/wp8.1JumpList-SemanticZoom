cd36199bb6fc5bd00ef28603cf47dda9748d4fd5
liupeng <79144876@qq.com>
2014-04-16 09:34:29 +0800

    init

---
+++ App3/DataModel/HouseGuest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App3.DataModel
+{
+    public class HouseGuest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+
+}
